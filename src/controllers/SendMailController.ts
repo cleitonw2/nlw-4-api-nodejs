a996cc2410e11c83d85564c84346aad20ccfb67b
Cleiton <cleitonwoycik@outlook.com>
2021-02-25 22:09:35 -0300

    aula 4 envio do email

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,10 +1,13 @@
 import { Request as Req, Response as Res } from 'express';
 import { getCustomRepository } from 'typeorm';
+import { resolve } from 'path';
 import { SurveysRepository } from '../repositories/SurveysRepository';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 import { UsersRepository } from '../repositories/UserRepository';
 import SendMailService from '../services/SendMailService';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 class SendMailController {
 
@@ -25,15 +28,34 @@ class SendMailController {
         if (!survey)
             return res.status(400).json({ error: 'Survey does not exists' });
 
-        //salvar as informações na tabela surveyUser
+        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description,
+            user_id: user.id,
+            link: process.env.URL_MAIL
+        }
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: [{ user_id: user.id }, { value: null }],
+            relations: ["user", "survey"],
+        });
+
+        if(surveyUserAlreadyExists) {
+            await SendMailService.execute(email, survey.title, variables, npsPath);
+            return res.json(surveyUserAlreadyExists)
+        }
+
         const surveyUser = surveysUsersRepository.create({
             user_id: user.id,
             survey_id
         });
 
         await surveysUsersRepository.save(surveyUser);
-        //enviar email para o usuário
-        await SendMailService.execute(email, survey.title, survey.description);
+
+        await SendMailService.execute(email, survey.title, variables, npsPath);
 
         return res.json(surveyUser)
     }
