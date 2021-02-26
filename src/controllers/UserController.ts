9cafd81404c0aecbe718907ade2434c557d6a498
Cleiton <cleitonwoycik@outlook.com>
2021-02-26 14:23:04 -0300

    validação com yup

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,22 @@
 import { Request as Req, Response as Res } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UserRepository';
+import * as yup from 'yup';
+
 class UserController {
     async create(req: Req, res: Res) {
         const { name, email } = req.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required()
+        });
+
+        try {
+            await schema.validate(req.body, { abortEarly: false });
+        } catch (error) {
+            return res.status(400).json(error);
+        }
 
         const userRepository = await getCustomRepository(UsersRepository);
 
@@ -27,3 +40,13 @@ class UserController {
 }
 
 export { UserController };
+
+/**
+ *  // if (! await schema.isValid(req.body))
+        //     return res.status(400).json({ error: "Validation failed!" });
+-----------------------------------------------------------------------------
+     const schema = yup.object().shape({
+            name: yup.string().required("nome é obrigatório"),
+            email: yup.string().email().required("email incorreto")
+        });
+ */
